e24960d42182341b1c9568a5f472583c493dd22a
MrTsepa <tsepa.stas@gmail.com>
2015-10-06 15:24:04 +0300

    hw1

--- hw1/complex.cpp
+++ hw1/complex.cpp
@@ -1,47 +1,46 @@
-#include <iostream>
-
 using namespace std;
 
-class complex {
+class C {
 	double re, im;
-	public:
-	complex(double a, double b) {re = a; im = b;}
-	complex(double a) {re = a; im = 0;}
-	complex(){re = 0; im = 0;}
+public:
+	C(double a, double b) {re = a; im = b;}
+	C(double a) {re = a; im = 0;}
+	C(){}
 
 	double abs() {return sqrt(re*re+im*im);}
 	
-	complex operator+ (complex a, complex b)
-	{
-		complex res;
-		res.re = a.re + b.re;
-		res.im = a.im + b.im;
-		return res;
-	}	
-	complex operator - (complex a, complex b)
-	{
-		complex res;
-		res.re = a.re - b.re;
-		res.im = a.im - b.im;
-		return res;
+	C operator+ (C &a) {
+		return C(a.re + re, a.im + im);
 	}
-	complex operator / (complex a, complex b)
-	{
-		complex res;
-		res.re = a.re / b.re;
-		res.im = a.im / b.im;
-		return res;
+	C operator- (C &a) {
+		return C(re - a.re, im - a.im);
 	}
-	complex operator * (complex a, complex b)
-	{
-		complex res;
-		res.re = a.re * b.re;
-		res.im = a.im * b.im;
-		return res;
+	C operator* (C &a) {
+		return C(a.re*re - a.im*im, a.re*im + a.im*re);
 	}
+	C operator/ (C &a) {
+		C x(a.re*re + a.im*im, a.re*im - a.im*re);
+		x.re /= (a.re*a.re + a.im*a.im);
+		x.im /= (a.re*a.re + a.im*a.im);
+		return x;
+	}
+	bool operator == (C &a) {
+		return (this->re == a.re) and (this->im == a.im);
+	}
+	bool operator != (C &a) {
+		return (this->re != a.re) or (this->im != a.im);
+	}
+	friend ostream &operator<<(ostream &os, C obj);
 };
 
-int main()
-{
-	return 0;
+ostream &operator<<(ostream &os, C obj){
+	os << obj.re << " " << obj.im;
+	return os;
 }
+
+C operator+(int i, C b) {return C(i) + b;}
+C operator-(int i, C b) {return C(i) - b;}
+C operator*(int i, C b) {return C(i) * b;}
+C operator/(int i, C b) {return C(i) / b;}
+bool operator==(int i, C b) {return C(i) == b;}
+bool operator!=(int i, C b) {return C(i) != b;}
