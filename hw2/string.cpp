c28bd24de7a237b189107aa2bccf6b2a53257e07
MrTsepa <tsepa.stas@gmail.com>
2015-10-14 12:27:03 +0300

    OK

--- hw2/string.cpp
+++ hw2/string.cpp
@@ -16,33 +16,36 @@ public:
 	S(const char*);
 	S(const string& A){
 		len = A.length();
-		pStr = new char[len];
+		pStr = new char[len+1];
 		for(int i = 0; i < len; i++){
 			pStr[i] = A[i];
 		}
+		pStr[len] = '\0';
 	}
 	S(const S&);
-	S() {pStr = NULL; len = 0;}
+	S() {pStr = new char; len = 0; pStr[len] = '\0';}
 	
 	const char* str() const{return pStr;}
 	size_t length() const{return len;}
 	
 	S operator+(const S &obj) const{
-		char* tmp = new char[len + obj.length()];
+		char* tmp = new char[len + obj.length()+1];
 		for (int i = 0; i < len; i++)
 			tmp[i] = pStr[i];
 		for (int i = 0; i < obj.length(); i++)
 			tmp[i + len] = obj.str()[i];
+		tmp[len + obj.length()] = '\0';
 		return S(tmp);
 	}
 	S& operator+=(const S &obj){
 		S tmp = (*this) + obj;
 		len = tmp.length();
 		delete[] pStr;
-		pStr = new char[len];
+		pStr = new char[len+1];
 		for(int i = 0; i < len; i++){
 			pStr[i] = tmp[i];
 		}
+		pStr[len] = '\0';
 		return (*this);
 	}
 	
@@ -53,27 +56,32 @@ public:
 };
 
 S::S(char a){
-	pStr = new char;
+	pStr = new char[2];
 	*pStr = a;
 	len = 1;
+	pStr[len] = '\0';
 }
 
 S::S(const char* str){
 	len = strlen(str);
-	pStr = new char[len];
+	pStr = new char[len+1];
 	for (int i = 0; i < len; i++)
 		pStr[i] = str[i];
+	pStr[len] = '\0';
 }
 
 S::S(const S &obj){
 	len = obj.len;
-	pStr = new char[len];
+	pStr = new char[len+1];
 	for(int i = 0; i < len; i++)
 		pStr[i] = obj.pStr[i];
+	pStr[len] = '\0';
 }
 
 ostream& operator<<(ostream& os, const S &obj){
-	if (obj.pStr) os << obj.pStr;
-	else os << "";
+//	if (obj.pStr) os << obj.pStr;
+//	else os << "";
+	for (int i = 0; i < obj.len; i++)
+		os << obj[i];
 	return os;
 }
