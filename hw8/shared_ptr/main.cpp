379f3fa31b3e50e9af8c2834972579ce51b394f2
Stas <tsepa.stas@gmail.com>
2016-03-14 15:08:44 +0300

    almost working

--- hw8/shared_ptr/main.cpp
+++ hw8/shared_ptr/main.cpp
@@ -14,7 +14,51 @@ int main()
 	if ((p2) and (p2.use_count() == 1))
 		std::cout << "OK 2" << std::endl;
 	shared_ptr<int> p3(std::move(p2));
-	if ((p2.use_count() == 0) and (p3.use_count() == 1))
+	if ((p2.use_count() == 0) and (p3.use_count() == 1) and !(p2) and (*p3 == 2))
 		std::cout << "OK 3" << std::endl;
+	shared_ptr<int> p4(p3);
+	if ((p3.use_count() == 2) and (p4.use_count() == 2) and (*p3 == 2) and (*p4 == 2))
+		std::cout << "OK 4" << std::endl;
+	shared_ptr<int> p5(p4);
+	if ((p3.use_count() == 3 and p5.use_count() == 3) and (*p5 == 2))
+		std::cout << "OK 5" << std::endl;
+	shared_ptr<int> p6(std::move(p3));
+	if ((p3.use_count() == 0) and (p6.use_count() == 3))
+		std::cout << "OK 6" << std::endl;
+	shared_ptr<int> p7(std::move(p1));
+	if ((p7.use_count() == 0))
+		std::cout << "OK 7" << std::endl;
+	p7 = std::move(p6);
+//	p6.print(); p7.print();
+//it's ok
+	std::cout << "OK 8" << std::endl;
+	p6 = p7;
+//	p5.print(); p6.print(); p7.print();
+	std::cout << "OK 9" << std::endl;
+	p3.reset(new int(3));
+	p5 = p3;
+//	p3.print(); p5.print(); p6.print();
+	std::cout << "OK 10" << std::endl;
+	p5.swap(p6);
+//	p3.print(); p5.print(); p6.print();
+	std::cout << "OK 11" << std::endl;
+	p1.reset(nullptr);
+	p2 = p1;
+	p3 = std::move(p2);
+	p4.swap(p3);
+//	p1.print(); p2.print(); p3.print(); p4.print();
+	std::cout << "OK 12" << std::endl;
+
+	weak_ptr<int> w1;
+	weak_ptr<int> w2(w1);
+	weak_ptr<int> w3(std::move(w2));
+	weak_ptr<int> w4(p3);
+	if (w4.use_count() == 3)
+		std::cout << "OK 13" << std::endl;
+	p3.reset(new int);
+	p5.reset(nullptr);
+	p7.reset(nullptr);
+	if (w4.use_count() == 0 and w4.expired())
+		std::cout << "OK 14" << std::endl;
 	return 0;
 }
--- hw8/shared_ptr/shared_ptr.h
+++ hw8/shared_ptr/shared_ptr.h
@@ -7,67 +7,247 @@
 namespace student {
 
 template <class T>
+class weak_ptr;
+
+template <class T>
 class shared_ptr {
 	T* ptr;
-	std::vector<shared_ptr<T>* > * shared_arr;
-	typename std::vector<shared_ptr<T>* >::iterator pos;
+	std::list<shared_ptr<T>* > * shared_arr;
+	typename std::list<shared_ptr<T>* >::iterator pos;
 public:
 	shared_ptr() {
 		ptr = nullptr;
-		shared_arr = new std::vector<shared_ptr<T>* >;
+		shared_arr = new std::list<shared_ptr<T>* >;
 		pos = shared_arr->end();
 	}
 		//создание пустого объекта, который ничем не владеет
 	shared_ptr(T* pointer){
-		ptr = pointer;
-		shared_arr = new std::vector<shared_ptr<T>* >;
-		shared_arr->push_back(this);
-		pos = shared_arr->begin();
+		if (pointer == nullptr) {
+			ptr = nullptr;
+			shared_arr = new std::list<shared_ptr<T>* >;
+			pos = shared_arr->end();
+		} else {
+			ptr = pointer;
+			shared_arr = new std::list<shared_ptr<T>* >;
+			shared_arr->push_back(this);
+			pos = shared_arr->begin();
+		}
 	}
 		//создание объекта, владеющего pointer
 	shared_ptr(shared_ptr<T> &&other) {
-		ptr = other.ptr;
-		other.ptr = nullptr;
-		other.shared_arr->erase(pos);
-		shared_arr = other.shared_arr;
-		other.shared_arr = new std::vector<shared_ptr<T>* >;
-		shared_arr->push_back(this);
-		pos = shared_arr->end()--;
+		if (other) {
+			ptr = other.ptr;
+			other.ptr = nullptr;
+			other.shared_arr->erase(other.pos);
+			shared_arr = other.shared_arr;
+			other.shared_arr = new std::list<shared_ptr<T>* >;
+			other.pos = other.shared_arr->end();
+			shared_arr->push_back(this);
+			pos = shared_arr->end();
+			pos--;
+		} else {
+			ptr = nullptr;
+			shared_arr = new std::list<shared_ptr<T>* >;
+			pos = shared_arr->end();
+		}
 	}
-
-	//rvalue копирование
-	shared_ptr(shared_ptr<T> const &other); //конструктор копирования, реализует равные права на владения обоими shared_ptr
-	~shared_ptr(){}                    //разрушение объекта, которым владеет этот shared_ptr, если он больше никому не принадлежит
-	T& operator * ();             //операторы разыменовывания
-	T* operator -> ();
-	shared_ptr& operator =(shared_ptr<T> &&x);      //присваивание rvalue
-	shared_ptr& operator =(shared_ptr<T> const&x);  //присваивание (с разделением прав)
-	void reset(T *p);                                  //смена владения с разрушением по необходимости предыдущего объекта
-	T* get();                                       //необходимо отдать сырой указатель на объект, которым владеем
-	void swap(shared_ptr<T> &x);                    //необходимо поменять местами объекты, которыми владеют this и x без разрушения и копирования оных
+		//rvalue копирование
+	shared_ptr(shared_ptr<T> const &other){
+		if (other.ptr != nullptr) {
+			ptr = other.ptr;
+			shared_arr = other.shared_arr;
+			shared_arr->push_back(this);
+			pos = shared_arr->end();
+			pos--;
+		} else {
+			ptr = nullptr;
+			shared_arr = new std::list<shared_ptr<T>* >;
+			pos = shared_arr->end();
+		}
+	}
+		//конструктор копирования, реализует равные права на владения обоими shared_ptr
+	~shared_ptr() {
+		if (*this) {
+			if (use_count() == 1) {
+				delete ptr;
+				delete shared_arr;
+			}
+		} else
+			delete shared_arr;
+	}
+		//разрушение объекта, которым владеет этот shared_ptr, если он больше никому не принадлежит
+	T& operator * () {return *ptr;}             //операторы разыменовывания
+	T* operator -> () {return ptr;}
+	shared_ptr& operator =(shared_ptr<T> &&other) {
+		if (use_count() == 1) {
+			delete ptr;
+			delete shared_arr;
+		}
+		else if (use_count() != 0) {
+			shared_arr->erase(pos);
+		}
+		if (other) {
+			ptr = other.ptr;
+			other.ptr = nullptr;
+			other.shared_arr->erase(other.pos);
+			shared_arr = other.shared_arr;
+			other.shared_arr = new std::list<shared_ptr<T>* >;
+			other.pos = other.shared_arr->end();
+			shared_arr->push_back(this);
+			pos = shared_arr->end();
+			pos--;
+		}
+		else {
+			ptr = nullptr;
+			shared_arr = new std::list<shared_ptr<T>* >;
+			pos = shared_arr->end();
+		}
+		return *this;
+	}
+		//присваивание rvalue
+	shared_ptr& operator =(shared_ptr<T> const &other) {
+		if (use_count() == 1) {
+			delete ptr;
+			delete shared_arr;
+		}
+		else if (use_count() != 0) {
+			shared_arr->erase(pos);
+		}
+		if (other.ptr != nullptr) {
+			ptr = other.ptr;
+			shared_arr = other.shared_arr;
+			shared_arr->push_back(this);
+			pos = shared_arr->end();
+			pos--;
+		} else {
+			ptr = nullptr;
+			shared_arr = new std::list<shared_ptr<T>* >;
+			pos = shared_arr->end();
+		}
+		return *this;
+	}
+		//присваивание (с разделением прав)
+	void reset(T *pointer) {
+		if (use_count() == 1) {
+			delete ptr;
+			delete shared_arr;
+		}
+		else if (use_count() != 0) {
+			shared_arr->erase(pos);
+		}
+		if (pointer == nullptr) {
+			ptr = nullptr;
+			shared_arr = new std::list<shared_ptr<T>* >;
+			pos = shared_arr->end();
+		} else {
+			ptr = pointer;
+			shared_arr = new std::list<shared_ptr<T>* >;
+			shared_arr->push_back(this);
+			pos = shared_arr->begin();
+		}
+	}
+		//смена владения с разрушением по необходимости предыдущего объекта
+	T* get() {return ptr;}  //необходимо отдать сырой указатель на объект, которым владеем
+	void swap(shared_ptr<T> &other) {
+		if (*this)
+			shared_arr->erase(pos);
+		if (other)
+			other.shared_arr->erase(other.pos);
+		std::swap(shared_arr, other.shared_arr);
+		std::swap(ptr, other.ptr);
+		if (*this) {
+			shared_arr->push_back(this);
+			pos = shared_arr->end()--;
+		}
+		else
+			pos = shared_arr->end();
+		if (other) {
+			other.shared_arr->push_back(&other);
+			other.pos = other.shared_arr->end()--;
+		} else
+			other.pos = other.shared_arr->end();
+	}
+		//необходимо поменять местами объекты, которыми владеют this и x
 	operator bool() {return (ptr != nullptr);}
 	//приведение к типу bool. true, если объект, которым владеем существует, false иначе
 	int use_count() {
 		return shared_arr->size();
 	}//возвращает количество shared_ptr, разделяющих владение, включая данный};
+	void print() {
+		std::cout << ptr << std::endl << use_count() << std::endl;
+	}
+
+	friend weak_ptr<T>;
 };
 
 template<class T>
 class weak_ptr
 {
+	T* ptr;
+	std::list<shared_ptr<T> *> *master_shared_arr;
 public:
-	weak_ptr();
-	weak_ptr(weak_ptr const & other);
-	weak_ptr(weak_ptr && other);
-	weak_ptr(shared_ptr<T> const &master); 		//создание weak_ptr, разделяющего владение с shared
-	~weak_ptr();
-	weak_ptr& operator = (weak_ptr const & other);
-	weak_ptr& operator = (weak_ptr && other);
-	void swap (weak_ptr &other); 				//обмен без копирования и стирания
-	void reset();								//сброс владения
-	int use_count(); 							//число shared_ptr владеющих объектом
-	bool expired();								//проверка на валидность. true, если объект "испортился", false, если объект валиден
-	shared_ptr<T> lock();						//возвращает shared_ptr, владеющий тем же объектом
+	weak_ptr() {
+		ptr = nullptr;
+		master_shared_arr = nullptr;
+	}
+
+	weak_ptr(weak_ptr const & other) {
+		ptr = other.ptr;
+		master_shared_arr = other.master_shared_arr;
+	}
+
+	weak_ptr(weak_ptr && other) {
+		ptr = other.ptr;
+		master_shared_arr = other.master_shared_arr;
+		other.ptr = nullptr;
+		other.master_shared_arr = nullptr;
+	}
+
+	weak_ptr(shared_ptr<T> const &master) {
+		ptr = master.ptr;
+		master_shared_arr = master.shared_arr;
+	}
+		//создание weak_ptr, разделяющего владение с shared
+	~weak_ptr() {;}
+	weak_ptr& operator = (weak_ptr const & other) {
+		ptr = other.ptr;
+		master_shared_arr = other.ptr;
+		return *this;
+	}
+
+	weak_ptr& operator = (weak_ptr && other) {
+		ptr = other.ptr;
+		master_shared_arr = other.master_shared_arr;
+		other.ptr = nullptr;
+		other.master_shared_arr = nullptr;
+		return *this;
+	}
+
+	void swap (weak_ptr &other) {
+		std::swap(ptr, other.ptr);
+		std::swap(master_shared_arr, other.master_shared_arr);
+	}
+		//обмен без копирования и стирания
+	void reset() {
+		ptr = nullptr;
+		master_shared_arr = nullptr;
+	}
+		//сброс владения
+	int use_count() {return master_shared_arr->size();} 	//число shared_ptr владеющих объектом
+	bool expired() {
+		return (ptr == nullptr);
+	}
+		//проверка на валидность. true, если объект "испортился", false, если объект валиден
+	shared_ptr<T> lock() {
+		if (expired()) {
+			shared_ptr<T> tmp;
+			return tmp;
+		} else {
+			shared_ptr<T> tmp(*(master_shared_arr->begin()));
+			return tmp;
+		}
+	}
+		//возвращает shared_ptr, владеющий тем же объектом
 };
 
 }
